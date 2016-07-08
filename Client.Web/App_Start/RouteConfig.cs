ec8f1ef5dfe70f928e2473b56b3338b7fc4c7baf
Леонов Артем <rash.scum@gmail.com>
2016-07-08 18:34:17 +0300

    добавил веб-приложение для SPA

---
+++ Client.Web/App_Start/RouteConfig.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Client.Web
+{
+    public class RouteConfig
+    {
+        public static void Configure(RouteCollection routes)
+        {
+            routes.LowercaseUrls = true;
+            routes.MapRoute("Default", "{*.}", new { controller = "Home", action = "Index" });
+        }
+    }
+}
---
+++ Client.Web/App_Start/Startup.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Routing;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Client.Web.Startup))]
+
+namespace Client.Web
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            RouteConfig.Configure(RouteTable.Routes);
+        }
+    }
+}
