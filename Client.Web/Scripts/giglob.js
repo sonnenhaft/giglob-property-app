bcd4df1c7889a260fc8a667e0b91fadf6579ea8e
Artem Valiulin <resp.by@gmail.com>
2016-07-12 13:23:57 +0300

    move giglob to top level, fix paths

--- Client.Web/Scripts/giglob.js
+++ Client.Web/Scripts/giglob.js
@@ -1,7 +1,6 @@
 angular.module('giglob-app').directive('giglob', function() {
     return {
-        restrict: 'E',
-        templateUrl: 'giglob.html',
+        templateUrl: '/giglob.html',
         link: function($scope) {
             
         }
