0c943fd1a5a93cf3a0e2ead52ad2d30e698480dd
Леонов Артем <rash.scum@gmail.com>
2016-07-12 17:20:29 +0300

    добавил сущность пользователя

---
+++ Domain/Entities/Implementation/User.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.Identity;
+
+namespace Domain.Entities.Implementation
+{
+    public class User: IUser<long>, IAggregateRootEntity<long>
+    {
+        public long Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Email { get; set; }
+    }
+}
