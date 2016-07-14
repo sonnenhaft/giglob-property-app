a352aed41882fe87d06253656ed7044894102664
bibragimov <xero-rock@hotmail.com>
2016-07-14 16:39:25 +0300

    add metrobranches.sql

---
+++ Domain.Persistence.EntityFramework/SQL/metrobranches.sql
@@ -0,0 +1,18 @@
+﻿IF NOT EXISTS(SELECT NULL FROM [dbo].[MetroBranches])
+BEGIN
+  DECLARE @name INT;
+select @name = Id from [dbo].[Cities] WHERE Name='Москва';
+ INSERT INTO [dbo].[MetroBranches]
+            ([HexColor],[CityId])
+     VALUES
+	 ('ffff00',@name),
+     ('6aa84f',@name),
+     ('ff9900',@name),
+     ('9fc5e8',@name),
+     ('b7b7b7',@name),
+     ('0000ff',@name),
+     ('a64d79',@name),
+     ('00ff00',@name),
+     ('ff0000',@name),
+     ('783f04',@name)		
+END
