e4951b6a7fcabca05a4f0d54e4ea4f9e52858dbb
Леонов Артем <rash.scum@gmail.com>
2016-07-18 17:30:35 +0300

    подготовил клиентский интерфейс для создания нового предложения объекта недвижимости

---
+++ Client.Api.v1/Controllers/PropertyOfferController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Http;
+using Client.Api.v1.Models.Models.Common.ResponseExamples;
+using Client.Api.v1.Models.Models.PropertyOffer;
+using SwaggerResponseExampleModule;
+
+namespace Client.Api.v1.Controllers
+{
+    public class PropertyOfferController: ApiController
+    {
+        [HttpPost]
+        [SwaggerResponseExampleProvider(typeof(SuccessResponseExample))]
+        public IHttpActionResult Create(PropertyOfferCreateRequestModel reqModel)
+        {
+            return Ok();
+        }
+    }
+}
