963e7df257a1e902bf7bc3ba9a7252c8060df9da
bibragimov <xero-rock@hotmail.com>
2016-07-18 12:21:36 +0300

    Aвтокомплит для станций метро, модели, QueryHandler

---
+++ Client.Api.v1.Models/Models/City/MetroStationModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Client.Api.v1.Models.Models.City
+{
+    public class MetroStationModel
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public ICollection<MetroBranchModel> Branches { get; set; }
+    }
+}
---
+++ Domain/Entities/Implementation/City/Queries/City_GetAllMetroStationQuery.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CQRS;
+using Domain.Repositories;
+
+namespace Domain.Entities.Implementation.City.Queries
+{
+    public class City_GetAllMetroStationQuery : IQuery
+    {
+        public long Id { get; set; }
+
+        public City_GetAllMetroStationQuery(long id)
+        {
+            Id = id;
+        }
+    }
+
+    public class City_GetAllMetroStationQueryHandler : IQueryHandler<City_GetAllMetroStationQuery, IEnumerable<MetroStation>>
+    {
+        private readonly ICityRepository _cityRepository;
+
+        public City_GetAllMetroStationQueryHandler(ICityRepository cityRepository)
+        {
+            _cityRepository = cityRepository;
+        }
+
+        public IEnumerable<MetroStation> Handle(City_GetAllMetroStationQuery query)
+        {
+            List<MetroStation> metroStations = new List<MetroStation>();
+
+            var city = _cityRepository.Get(query.Id);
+            if (city != null)
+            {
+                metroStations = city.MetroStations.ToList();
+            }
+
+            return metroStations;
+        }
+    }
+}
