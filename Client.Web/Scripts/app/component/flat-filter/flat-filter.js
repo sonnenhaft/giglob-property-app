4775c2dc16147168dce7e2681fc686fd52449f21
Vitali Makhnach <vmakhnach@edmunds.com>
2016-07-22 09:55:18 +0300

    Add multiselect dropdown

--- Client.Web/Scripts/app/component/flat-filter/flat-filter.js
+++ Client.Web/Scripts/app/component/flat-filter/flat-filter.js
@@ -1,10 +1,14 @@
-angular.module('component.flat-filter', ['component.multiselect-list']).directive('flatFilter', function() {
+angular.module('component.flat-filter', [
+    'component.multiselect-list',
+    'component.multiselect-dropdown'
+]).directive('flatFilter', function() {
     return {
         replace: true,
         templateUrl: 'app/component/flat-filter/flat-filter.html',
         scope: true,
         link: function ($scope) {
             $scope.selectedRoomCount = [];
+            $scope.selectedStations = [];
             $scope.roomCount = [{name: '1', value: '1'}, {name: '2', value: '2'}, {name: '3', value: '3'}, {name: '4+', value: '4+'}];
         }
     };
