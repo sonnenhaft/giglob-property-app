d96dff92b295459173091c38e05451c9d348c510
Artem Valiulin <resp.by@gmail.com>
2016-07-23 19:46:30 +0300

    add missing app deps, refactor city popup module, update styles

---
+++ Client.Web/Scripts/app/component/city-popup/city-popup.js
@@ -0,0 +1,53 @@
+angular.module('component.city-popup', ['mm.foundation.modal', 'LocalStorageModule']).directive('cityPopup', function($modal, localStorageService){
+    return {
+        restrict: 'E',
+        scope: {},
+        link: function($scope) {
+            localStorageService.set('city', 'Moscow');
+
+            if(localStorageService.get('city')) {
+                var modalInstance = $modal.open({
+                    templateUrl: '/app/component/city-popup/city-popup.html',
+                    controller: 'cityPopupCtrl',
+                    windowClass: 'choose-city',
+                    backdrop: 'static',
+                    closeOnClick: false,
+                    resolve: {
+                        items: function () {
+                            console.log('123');
+                        }
+                    }
+                });
+            }
+        }
+    };
+}).controller('cityPopupCtrl', function($scope, $http, $modalInstance, localStorageService) {
+    $scope.changeCity = function() {
+        //TODO вынести в отдельный API сервис
+        $http.get('https://giglobapi.igstest.ru/v1/home/getdata').then(function(res){
+            $scope.cities = res.data.cities;
+            $scope.autocomplete = true;
+        });
+
+    };
+
+    $scope.chooseCity = function(name) {
+        var arr = [];
+        $scope.cities.find(function(el) {
+            if(el.name.indexOf(name) !== -1){
+                arr.push(el);
+            }
+        });
+        console.log(arr);
+        return arr;
+    };
+
+    $scope.ok = function() {
+        $modalInstance.close();
+    };
+
+    $scope.selected = function(name) {
+        localStorageService.set('city', name);
+        $scope.ok();
+    };
+});
--- Client.Web/Scripts/app/giglob.js
+++ Client.Web/Scripts/app/giglob.js
@@ -4,27 +4,12 @@ angular.module('giglob-app',[
     'mm.foundation',
     "component.gheader",
     'component.router',
-    'component.cityPopup'
+    'component.city-popup',
     'component.carousel'
 ]).directive('giglob', function() {
     return {
         templateUrl: '/app/giglob.html',
-        controller: function($scope,$modal,localStorageService){
-            if(!localStorageService.get('city')){
-                var modalInstance = $modal.open({
-                    templateUrl: '/app/component/cityPopup/cityPopup.html',
-                    controller:'cityPopupCtrl',
-                    windowClass:'choose-city',
-                    resolve: {
-                        items: function () {
-                            console.log('123');
-                        }
-                    }
-                });
-            }
-        },
-        link: function($scope) {
-
-        }
+        controller: function($scope) {},
+        link: function($scope) {}
     };
 });
