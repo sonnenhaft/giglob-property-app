9404621e1717cf894ffefe9c55d23e5dd086d4c5
artem_valiulin <resp.by@gmail.com>
2016-07-26 02:37:40 +0300

    Fix styles

--- Client.Web/Scripts/app/component/config/router/router-config.js
+++ Client.Web/Scripts/app/component/config/router/router-config.js
@@ -83,9 +83,9 @@ angular.module('component.config.router', ['ui.router']).config(function($stateP
                             coordinates: [37.715175, 55.833436]
                         },
                         properties: {
-                            iconContent: 'Заголовок метки',
-                            hintContent: 'Ховер хинт',
-                            balloonContent: 'Разметка для всплывающей подсказки'
+                            iconContent: '', // Заголовок метки
+                            hintContent: '', //Ховер хинт
+                            balloonContent: ''//Разметка для всплывающей подсказки
                         }
                     },
                     images: [{
@@ -123,6 +123,10 @@ angular.module('component.config.router', ['ui.router']).config(function($stateP
         .state('home', {
             url: "/",
             templateUrl: 'app/component/config/router/main-page.html'
+        })
+        .state('add', {
+            url: "/add",
+            templateUrl: 'app/component/config/router/add.html'
         });
 
 }).constant('EXCLUDED_DEMO_ROUTERS', [
