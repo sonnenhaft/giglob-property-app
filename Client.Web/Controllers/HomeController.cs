ce7f26bbbd3d1625c1a7885bf43d4677a275d464
Артем Леонов < >
2016-07-25 17:21:52 +0300

    добавил адрес апи в главную страницу веб-приложения

--- Client.Web/Controllers/HomeController.cs
+++ Client.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Mvc;
+﻿using System.Configuration;
+using System.Web.Mvc;
 
 namespace Client.Web.Controllers
 {
@@ -6,7 +7,9 @@ namespace Client.Web.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            var url = ConfigurationManager.AppSettings["ApiUrl"];
+
+            return View(model: url);
         }
     }
 }
