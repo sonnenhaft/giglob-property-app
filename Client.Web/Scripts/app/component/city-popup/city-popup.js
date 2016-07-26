45565d6765fa9c52474277612a01f17c298a7827
Artem Valiulin <resp.by@gmail.com>
2016-07-26 19:05:58 +0300

    refactor city-popup component

--- Client.Web/Scripts/app/component/city-popup/city-popup.js
+++ Client.Web/Scripts/app/component/city-popup/city-popup.js
@@ -1,54 +1,46 @@
-angular.module('component.city-popup', ['mm.foundation.modal', 'LocalStorageModule'])
-    .directive('cityPopup', function($modal, localStorageService){
-    return {
-        restrict: 'E',
-        scope: {
-            ignoreLocalStorageValue: '=?'
-        },
-        link: function($scope) {
-            if(!localStorageService.get('city')) {
-                var modalInstance = $modal.open({
-                    templateUrl: 'app/component/city-popup/city-popup.html',
-                    windowClass: 'choose-city',
-                    backdrop: 'static',
-                    closeOnClick: false,
-                    keyboard: false,
-                    resolve: {
-                        items: function () {
-                            console.log('123');
-                        }
-                    },
-                    controller: function($scope, $http, $modalInstance, localStorageService) {
-                        $scope.changeCity = function () {
-                            //TODO вынести в отдельный API сервис
-                            $http.get('http://giglobapi.igstest.ru/v1/home/getdata').then(function (res) {
-                                $scope.cities = res.data.cities;
-                                $scope.autocomplete = true;
-                            });
-
-                        };
-
-                        $scope.chooseCity = function (name) {
-                            var regex = new RegExp('^' + name, 'i');
-
-                            return $scope.cities.filter(function (city) {
-                                return regex.test(city.name);
-                            });
-                        };
-
-                        $scope.ok = function () {
-                            localStorageService.set('city', 'Moscow');
-                            $modalInstance.close();
-                        };
-
-                        $scope.selected = function (name) {
-                            localStorageService.set('city', name);
-                            $modalInstance.close();
-                        };
+angular.module('component.city-popup', ['mm.foundation.modal', 'LocalStorageModule']).directive('cityPopup', function($modal, localStorageService){
+    return function($scope) {
+        if(!localStorageService.get('city')) {
+            $modal.open({
+                templateUrl: 'app/component/city-popup/city-popup.html',
+                windowClass: 'choose-city',
+                backdrop: 'static',
+                closeOnClick: false,
+                keyboard: false,
+                resolve: {
+                    items: function () {
+                        console.log('123');
                     }
-                });
-            }
+                },
+                controller: function($scope, $http, $modalInstance, localStorageService) {
+                    $scope.changeCity = function () {
+                        //TODO вынести в отдельный API сервис
+                        $http.get('http://giglobapi.igstest.ru/v1/home/getdata').then(function (res) {
+                            $scope.cities = res.data.cities;
+                            $scope.autocomplete = true;
+                        });
 
+                    };
+
+                    $scope.chooseCity = function (name) {
+                        var regex = new RegExp('^' + name, 'i');
+
+                        return $scope.cities.filter(function (city) {
+                            return regex.test(city.name);
+                        });
+                    };
+
+                    $scope.ok = function () {
+                        localStorageService.set('city', 'Moscow');
+                        $modalInstance.close();
+                    };
+
+                    $scope.selected = function (name) {
+                        localStorageService.set('city', name);
+                        $modalInstance.close();
+                    };
+                }
+            });
         }
-    };
+    }
 });
