279e026f8136bde159a3dd93b55bebbaca4b43a0
Artem Valiulin <resp.by@gmail.com>
2016-07-26 19:00:46 +0300

    remove cityPopupCtrl, disable ESC key closing

--- Client.Web/Scripts/app/component/city-popup/city-popup.js
+++ Client.Web/Scripts/app/component/city-popup/city-popup.js
@@ -6,48 +6,49 @@ angular.module('component.city-popup', ['mm.foundation.modal', 'LocalStorageModu
             ignoreLocalStorageValue: '=?'
         },
         link: function($scope) {
-
             if(!localStorageService.get('city')) {
                 var modalInstance = $modal.open({
                     templateUrl: 'app/component/city-popup/city-popup.html',
-                    controller: 'cityPopupCtrl',
                     windowClass: 'choose-city',
                     backdrop: 'static',
                     closeOnClick: false,
+                    keyboard: false,
                     resolve: {
                         items: function () {
                             console.log('123');
                         }
+                    },
+                    controller: function($scope, $http, $modalInstance, localStorageService) {
+                        $scope.changeCity = function () {
+                            //TODO вынести в отдельный API сервис
+                            $http.get('http://giglobapi.igstest.ru/v1/home/getdata').then(function (res) {
+                                $scope.cities = res.data.cities;
+                                $scope.autocomplete = true;
+                            });
+
+                        };
+
+                        $scope.chooseCity = function (name) {
+                            var regex = new RegExp('^' + name, 'i');
+
+                            return $scope.cities.filter(function (city) {
+                                return regex.test(city.name);
+                            });
+                        };
+
+                        $scope.ok = function () {
+                            localStorageService.set('city', 'Moscow');
+                            $modalInstance.close();
+                        };
+
+                        $scope.selected = function (name) {
+                            localStorageService.set('city', name);
+                            $modalInstance.close();
+                        };
                     }
                 });
             }
+
         }
-    };
-}).controller('cityPopupCtrl', function($scope, $http, $modalInstance, localStorageService) {
-    $scope.changeCity = function() {
-        //TODO вынести в отдельный API сервис
-        $http.get('http://giglobapi.igstest.ru/v1/home/getdata').then(function(res){
-            $scope.cities = res.data.cities;
-            $scope.autocomplete = true;
-        });
-
-    };
-
-    $scope.chooseCity = function(name) {
-        var regex = new RegExp('^' + name, 'i');
-
-        return $scope.cities.filter(function(city) {
-            return regex.test(city.name);
-        });
-    };
-
-    $scope.ok = function() {
-        localStorageService.set('city','Moscow');
-        $modalInstance.close();
-    };
-
-    $scope.selected = function(name) {
-        localStorageService.set('city', name);
-        $modalInstance.close();
     };
 });
