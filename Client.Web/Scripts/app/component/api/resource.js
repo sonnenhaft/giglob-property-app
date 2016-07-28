3542ab272b7b303e7f36cc500eb2a235a96eac6f
Mike Nikonov <lounge1k@gmail.com>
2016-07-28 11:19:54 +0300

    pictures  fix

--- Client.Web/Scripts/app/component/api/resource.js
+++ Client.Web/Scripts/app/component/api/resource.js
@@ -1,21 +1,21 @@
 ﻿angular.module("api.resource",["ngResource"])
     .factory("register", function ($resource) {
 
-        return  $resource('http://api.giglob.local/v1/user/register',
+        return  $resource('http://giglobapi.igstest.ru/v1/user/register',
             null,
             {
                 'save': { method:'POST'}
             });
     })
     .factory('login',function($resource){
-        return $resource('http://api.giglob.local/v1/user/signin',
+        return $resource('http://giglobapi.igstest.ru/v1/user/signin',
             null,
             {
                 'save' : {method:'POST'}
             });
     })
     .factory('confirm',function($resource){
-        return $resource('http://api.giglob.local/v1/user/confirmemail',
+        return $resource('http://giglobapi.igstest.ru/v1/user/confirmemail',
             null,
             {
                 'save' : {method:'POST'}
