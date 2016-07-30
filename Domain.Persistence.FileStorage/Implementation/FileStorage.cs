443c6d3f8dfe99f0824a82fd8fe589f8124b3208
Артем Леонов < >
2016-07-30 13:16:20 +0300

    заменил FileStream на File.Open

--- Domain.Persistence.FileStorage/Implementation/FileStorage.cs
+++ Domain.Persistence.FileStorage/Implementation/FileStorage.cs
@@ -29,7 +29,7 @@ namespace Domain.Persistence.FileStorage.Implementation
         public Stream Get(string virtualPath)
         {
             var fullPath = _virtualPathUtility.ConvertToFullPath(virtualPath);
-            var fs = new FileStream(fullPath, FileMode.Open, FileAccess.Read, FileShare.None);
+            var fs = File.Open(fullPath, FileMode.Open, FileAccess.Read, FileShare.None);
 
             return fs;
         }
