1cffaa75d282d803350cd5f4142760922a492517
Артем Леонов < >
2016-08-02 14:17:02 +0300

    фикс  неправильного мержа

--- Client.Api.v1.Models/Constants/RegexConstants.cs
+++ Client.Api.v1.Models/Constants/RegexConstants.cs
@@ -1,9 +1,9 @@
-﻿﻿using Domain.Entities.User.Implementation;
+﻿using Domain.Entities.User.Implementation;
 
 namespace Client.Api.v1.Models.Constants
 {
     public class RegexConstants
     {
-        public const string EmailRegex = @"^[a-z0-9\.\-_]+@[a-z0-9\-]+\.[a-z0-9]{2,16}$";
+        public const string EmailRegex = @"^[a-zA-Z0-9\.\-_]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9]{2,16}$";
     }
 }
