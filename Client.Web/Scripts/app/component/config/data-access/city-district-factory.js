1156df79547c7863eba87b2a218ed87c381fa0c4
Valiulin <resp.by@gmail.com>
2016-08-09 02:51:28 +0200

    #GIG-272 Создать объект. В списке городов города undefined и пустые строки

--- Client.Web/Scripts/app/component/config/data-access/city-district-factory.js
+++ Client.Web/Scripts/app/component/config/data-access/city-district-factory.js
@@ -1,19 +1,24 @@
-angular.module('component.config.data-access').factory('cityDistrictFactory',
-    function($resource, currentServer) {
+angular.module('component.config.data-access').factory('cityDistrictFactory', function ($resource, currentServer) {
     return $resource(currentServer + '/v1/home/getdata', null, {
         get: {
             method: 'GET',
             transformResponse: function (data) {
-                var cities = {};
-                data = JSON.parse(data);
-                data.cities.forEach(function(item){
-                    var dist = {};
-                    item.districts.forEach(function(item2){
-                        dist[item2.id] = item2.name;
-                    });
-                    cities[item.id] = {'name': item.name, 'districts': dist};
-                });
-                return cities;
+                var cities = JSON.parse(data).cities;
+                return {
+                    asArray: cities,
+                    asMap: cities.reduce(function (cities, city) {
+                        cities[city.id] = {
+                            id: city.id,
+                            name: city.name,
+                            districtsAsArray: city.districts,
+                            districts: city.districts.reduce(function (dist, district) {
+                                dist[district.id] = district.name;
+                                return dist;
+                            }, {})
+                        };
+                        return cities;
+                    }, {})
+                }
             }
         }
     });
--- Client.Web/Scripts/app/component/tab-section/tab-section.js
+++ Client.Web/Scripts/app/component/tab-section/tab-section.js
@@ -1,5 +1,7 @@
-angular.module('component.tab-section', ['ngSanitize', 'ngFileUpload']).directive('tabSection', function(addFlatTabs, cityDistrictFactory, $rootScope, Upload, currentServer) {
-
+angular.module('component.tab-section', [
+    'ngSanitize',
+    'ngFileUpload'
+]).directive('tabSection', function (addFlatTabs, cityDistrictFactory, $rootScope, Upload, currentServer) {
     return {
         restrict: 'E',
         scope: {
@@ -8,77 +10,33 @@ angular.module('component.tab-section', ['ngSanitize', 'ngFileUpload']).directiv
             tabCollectionType: '@'
         },
         templateUrl: 'app/component/tab-section/tab-section.html',
-        link: function($scope, $element) {
+        link: function ($scope) {
             $scope.addFlatTabs = addFlatTabs;
             $scope.uploadedFiles = [];
             $scope.model = [];
-
             $scope.data = {
                 cities: [],
                 districts: [],
-                stations: [
-                    {
-                        id: 1,
-                        name: 'Библиотек4а им. Ленина'
-                    },
-                    {
-                        id: 2,
-                        name: 'Кузминки'
-                    }
-                ],
-                buildCategories : [
-                    {
-                        id: 1,
-                        name: 'Многоквартирный дом'
-                    },
-                    {
-                        id: 2,
-                        name: 'Частный дом'
-                    }
-                ],
-                realEstateTypes: [
-                    {
-                        id: 1,
-                        name: 'Новостройка'
-                    },
-                    {
-                        id: 2,
-                        name: 'Вторичка'
-                    }
-                ]
+                stations: [{id: 1, name: 'Библиотек4а им. Ленина'}, {id: 2, name: 'Кузминки'}],
+                buildCategories: [{id: 1, name: 'Многоквартирный дом'}, {id: 2, name: 'Частный дом'}],
+                realEstateTypes: [{id: 1, name: 'Новостройка'}, {id: 2, name: 'Вторичка'}]
             };
 
             $scope.selectCity = function () {
-
-                var dist = $scope.cityDistricts[$scope.model.city.id].districts;
-
-                var distList = [];
-                for(var item in dist){
-                    distList.push({
-                        id: item,
-                        name: dist[item]
-                    });
-
-                }
-                $scope.data.districts= distList;
+                $scope.data.districts = $scope.cityDistricts[$scope.model.city.id].districtsAsArray;
             };
 
-            cityDistrictFactory.get().$promise.then(function(data){
-                $scope.cityDistricts = data;
-                for(var item in data){
-                    $scope.data.cities.push({
-                        id: item,
-                        name: data[item].name
-                    });
-                }
-
+            cityDistrictFactory.get().$promise.then(function (data) {
+                $scope.cityDistricts = data.asArray;
+                $scope.data.cities = data.asMap;
+                $scope.model.city = $scope.model.city || data.asArray[0];
                 $scope.selectCity();
             });
 
             $scope.uploadFiles = function (files, getMeta, type) {
                 if (files && files.length) {
-                    files.forEach(function(file) {
-                        if(getMeta) {
+                    files.forEach(function (file) {
+                        if (getMeta) {
                             file.formattedName = file.name.split('.')[0];
                             file.format = file.name.split('.').pop().toUpperCase();
                         }
@@ -87,16 +45,16 @@ angular.module('component.tab-section', ['ngSanitize', 'ngFileUpload']).directiv
                             data: {File: file, FileName: file.name},
                             headers: {'Authorization': 'Bearer ' + $rootScope.accessToken}
                         });
-                        upload.then(function(file) {
-                            if (type == 'doc'){
+                        upload.then(function (file) {
+                            if (type == 'doc') {
                                 $scope.model.push(file.data.id);
-                            }else{
+                            } else {
                                 $scope.model.push(file.data);
                                 $scope.setCoverModel();
                             }
 
-                        }, function(resp) {
-                        }, function(evt) {
+                        }, function (resp) {
+                        }, function (evt) {
                         });
                         $scope.uploadedFiles.push(file);
                     });
@@ -105,19 +63,19 @@ angular.module('component.tab-section', ['ngSanitize', 'ngFileUpload']).directiv
             };
 
             $scope.setCover = function (index, skip) {
-                if ($scope.uploadedFiles.length){
+                if ($scope.uploadedFiles.length) {
                     !skip && ($scope.uploadedFiles[$scope.lastCoverIndex || 0].isCover = false);
                     $scope.lastCoverIndex = index || 0;
                     $scope.uploadedFiles[$scope.lastCoverIndex].isCover = true;
                     $scope.isAdded = true;
-                }else{
+                } else {
                     $scope.isAdded = false;
                 }
 
             };
 
             $scope.setCoverModel = function (index, skip) {
-                if ($scope.model.length){
+                if ($scope.model.length) {
                     !skip && ($scope.model[$scope.lastCoverIndex || 0].isCover = false);
                     $scope.lastCoverIndex = index || 0;
                     $scope.model[$scope.lastCoverIndex].isCover = true;
@@ -129,10 +87,10 @@ angular.module('component.tab-section', ['ngSanitize', 'ngFileUpload']).directiv
                 $scope.uploadedFiles.splice(index, 1);
                 $scope.model.splice(index, 1);
 
-                if($scope.lastCoverIndex === index) {
+                if ($scope.lastCoverIndex === index) {
                     index = $scope.uploadedFiles.length <= index ? index - 1 : index;
                     $scope.setCover(index, true);
-                    if (type != 'doc'){
+                    if (type != 'doc') {
                         $scope.setCoverModel(index, true);
                     }
 
@@ -147,12 +105,12 @@ angular.module('component.tab-section', ['ngSanitize', 'ngFileUpload']).directiv
                 addFlatTabs[tabCollectionType][nexTabIndex].active = true;
             };
 
-            $scope.sendData = function(type) {
+            $scope.sendData = function (type) {
                 $rootScope.$broadcast('addFormSubmitted', type);
             };
 
             $scope.$on('objectSaved', function () {
-                if($scope.uploadedFiles) {
+                if ($scope.uploadedFiles) {
                     $scope.uploadedFiles = [];
                 }
             });
